f84e93c241faa459d4154cf5605eacfa7a3a2605
rafael.f.andrade <rafael.f.andrade@accenture.com>
2025-07-28 23:15:01 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -21,7 +21,7 @@ it('login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(selectorsList.usernameField).type('test')
     cy.get(selectorsList.passwordField).type('admin123')
-    cy,get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
+    cy,get.click()
+    cy.get
   })
 })
